bc9539a25241c497891667043e6f313c8661c1e1
Antonio <antonio.rodriguez024@gmail.com>
2021-12-27 17:19:09 -0600

    create a container

---
+++ my-app/src/components/anime-table/TableContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Table from "./Table"
+import KH from '../../res/gifs/KH.gif'
+
+export default class TableContainer extends Component {
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:4000/getAllAnime')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({data: json})
+            })
+    }
+
+    render() {
+        console.log(this.state)
+
+        return (
+            <div>
+                <div className='centered-row' style={{ paddingTop: 10, paddingBottom: 10 }}>
+                    <p className='table-title'>Anime Information</p>    
+                </div>
+
+                <div className='centered-row'>
+                    <img src={KH} alt='kingdom-hearts' />
+                </div>
+
+                <Table data={this.state.data} />
+            </div>    
+        )
+    }
+}
