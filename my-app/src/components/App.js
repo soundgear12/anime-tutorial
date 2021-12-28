18dd9cb8d02a8721748448bc2678be1925b53518
Antonio <antonio.rodriguez024@gmail.com>
2021-12-27 18:45:21 -0600

    menubar navigation

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import TableContainer from './anime-table/TableContainer';
+import Table from './anime-table/Table';
+import MenuBar from './navbar/MenuBar';
+import history from '../history';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 export default class App extends Component {
     render() {
         return (
-            <TableContainer />
+            <BrowserRouter history={history}>
+                <MenuBar />
+                <Routes>
+                    <Route exact path="/home" element={<TableContainer />} />
+                    <Route exact path="/test"  element={<Table />} />
+                </Routes>
+            </BrowserRouter>
         )
     }
 }
--- my-app/src/components/anime-table/Table.js
+++ my-app/src/components/anime-table/Table.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactTable from 'react-table-6';
+import Loadingbar from '../loader/LoadingBar'
 import 'react-table-6/react-table.css';
 
 const Table = ({ data }) => {
     if (!data || !data[0]) {
-        return null
+        return <Loadingbar />
     }
 
     const columns = Object.keys(data[0]).map(key => {
